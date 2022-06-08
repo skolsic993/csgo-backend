b7683156a0545df52812aedb16c70a1343eb4156
Miloš Stojsavljević <skolsic993@gmail.com>
2022-06-08 13:02:19 +0200

    Add on heroku

--- src/app.ts
+++ src/app.ts
@@ -5,17 +5,15 @@ import logger from './utils/logger';
 import routes from './routes';
 import deserializeUser from './middleware/deserializeUser';
 
-const port = config.get<number>('port');
+const port = process.env.PORT || 1337;
 
 const app = express();
-
 app.use(express.json());
 app.use(deserializeUser);
 
 app.listen(port, async () => {
-  logger.info(
-    `App is running at https://counter-strike-backend.herokuapp.com:${port}`
-  );
+  logger.info(`App is running at http://localhost:${port}`);
+
   await connect();
 
   routes(app);
