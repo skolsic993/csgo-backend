0b08111841fc6f36266bc5c79a7e10298acd6dce
Miloš Stojsavljević <skolsic993@gmail.com>
2022-06-08 12:45:17 +0200

    Add app to the heroku

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ import deserializeUser from './middleware/deserializeUser';
 const port = config.get<number>('port');
 
 const app = express();
+
 app.use(express.json());
 app.use(deserializeUser);
 
