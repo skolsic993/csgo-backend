ec8ed81fb681ad6e08bcc8a959be4b2ef4cd3845
Miloš Stojsavljević <skolsic993@gmail.com>
2022-06-12 18:47:02 +0200

    Add name to the response

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -13,6 +13,7 @@ export interface UserDocument extends mongoose.Document {
 
 const userSchema = new mongoose.Schema(
   {
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
