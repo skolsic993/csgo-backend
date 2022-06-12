0b7ada5a1214009c0a101d19ca3d848b3f76e3d8
Miloš Stojsavljević <skolsic993@gmail.com>
2022-06-12 18:40:45 +0200

    Remove username and put email instead

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -13,7 +13,7 @@ export interface UserDocument extends mongoose.Document {
 
 const userSchema = new mongoose.Schema(
   {
-    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
   },
   {
