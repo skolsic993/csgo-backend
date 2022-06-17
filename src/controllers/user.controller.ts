067015d11a6a751c9f81728117890b0853dd524c
Miloš Stojsavljević <skolsic993@gmail.com>
2022-06-17 19:01:51 +0200

    Blank

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -21,7 +21,11 @@ export async function createUserHandler(
       req
     );
 
-    res.cookie('express:jwt', accessToken, { httpOnly: true, secure: true });
+    res.cookie('express:jwt', accessToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',
+    });
     return res.send({ user, accessToken });
   } catch (error) {
     logger.error(error);
