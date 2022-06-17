46487903f57f74648072629226bf1b4effed171b
Miloš Stojsavljević <skolsic993@gmail.com>
2022-06-17 11:24:24 +0200

    Fix check auth

--- src/middleware/checkAuth.ts
+++ src/middleware/checkAuth.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 const jwt = require('jsonwebtoken');
 
 const checkAuth = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.cookies.jwt;
+  const token = req;
 
   console.log(token);
   return next();
