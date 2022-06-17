24365c9d947252d1712a2006f9ea4961ec8ac5ce
Miloš Stojsavljević <skolsic993@gmail.com>
2022-06-17 21:01:09 +0200

    Add token

--- src/middleware/checkAuth.ts
+++ src/middleware/checkAuth.ts
@@ -3,8 +3,11 @@ import { Request, Response, NextFunction } from 'express';
 const jwt = require('jsonwebtoken');
 
 const checkAuth = (req: Request, res: Response, next: NextFunction) => {
-  const token = req;
-
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) {
+    return res.sendStatus(401);
+  }
   return next();
 };
 
