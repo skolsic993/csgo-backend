992359d73efb279722b167e12c289b8913e35186
Miloš Stojsavljević <skolsic993@gmail.com>
2022-06-18 14:43:54 +0200

    remove secure

--- src/middleware/checkAuth.ts
+++ src/middleware/checkAuth.ts
@@ -3,11 +3,10 @@ import { Request, Response, NextFunction } from 'express';
 const jwt = require('jsonwebtoken');
 
 const checkAuth = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req;
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
 
-  res.send(authHeader);
-
-  if ('sa' === undefined) {
+  if (token === undefined) {
     return res.sendStatus(401);
   }
 
