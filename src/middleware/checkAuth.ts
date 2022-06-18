7a8c74c8875443436a0dc4fa163e41e4286366ed
Miloš Stojsavljević <skolsic993@gmail.com>
2022-06-18 13:26:06 +0200

    Add token

--- src/middleware/checkAuth.ts
+++ src/middleware/checkAuth.ts
@@ -6,11 +6,9 @@ const checkAuth = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
-  if (token === undefined) {
-    return res.sendStatus(503);
+  if (token) {
+    return next();
   }
-
-  return next();
 };
 
 export default checkAuth;
