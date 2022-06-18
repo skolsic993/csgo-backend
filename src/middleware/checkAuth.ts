ebb32fa2bb8dfb9b27c1878714a8090c7650aabe
Miloš Stojsavljević <skolsic993@gmail.com>
2022-06-18 11:22:34 +0200

    Fix signed in

--- src/middleware/checkAuth.ts
+++ src/middleware/checkAuth.ts
@@ -5,6 +5,7 @@ const jwt = require('jsonwebtoken');
 const checkAuth = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
+
   if (!token) {
     return res.sendStatus(401);
   }
